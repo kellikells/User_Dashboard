b4723a7a125140125ae43547c801546bc243112e
kelli <kellikells@gmail.com>
2020-10-15 18:06:24 -0700

    stylized search by name input form on dashboard.html and included empty input to show all users

--- apps/user_dashboard/views.py
+++ apps/user_dashboard/views.py
@@ -303,14 +303,17 @@ def search_by_name(request):
 
     # normal user level
     if request.POST['name'] and request.session['user_level'] == "0":
-
         users = DashboardUser.objects.filter(first_name__startswith = request.POST['name'])
- 
+        # if there are no users that fit the seach input OR search input is empty
+        if users.count() == 0 or request.POST['name'] == '':
+            users = DashboardUser.objects.all()
         return render(request, 'user_dashboard/table_normal.html', {'users': users})
     
     # admin level 
     else:
         users = DashboardUser.objects.filter(first_name__startswith = request.POST['name'])
- 
+        # if there are no users that fit the seach input OR search input is empty
+        if users.count() == 0 or request.POST['name'] == '':
+            users = DashboardUser.objects.all()
         return render(request, 'user_dashboard/table_admin.html', {'users': users})
 
